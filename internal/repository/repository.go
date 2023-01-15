aee0023f151e6f2b901fc4ed12d3fae4ae49af95
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-15 19:51:40 +0330

    repo registry register new repo

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,7 @@
 package repository
 
-import "github.com/amir79esmaeili/sms-gateway/internal/cfg"
-
 type Repository interface {
-	Configure(config *cfg.Config)
+	Configure(db interface{})
 	List() (interface{}, error)
 	Get(id interface{}) (interface{}, error)
 	Create(entity interface{}) (interface{}, error)
--- internal/repository/repository_registry.go
+++ internal/repository/repository_registry.go
@@ -1,1 +1,25 @@
 package repository
+
+import (
+	"reflect"
+)
+
+type Registry struct {
+	registry map[string]Repository
+}
+
+func NewRepositoryRegistry(repository ...Repository) *Registry {
+	r := &Registry{
+		registry: map[string]Repository{},
+	}
+
+	r.registerRepositories(repository)
+	return r
+}
+
+func (r *Registry) registerRepositories(repositories []Repository) {
+	for _, repository := range repositories {
+		repositoryName := reflect.TypeOf(repository).Elem().Name()
+		r.registry[repositoryName] = repository
+	}
+}
