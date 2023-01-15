a3dd2688917501ba598cce04e476d586abbaba99
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-15 21:19:36 +0330

    refactored repository interface

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,10 +1,21 @@
 package model
 
-import "time"
+import (
+	"context"
+	"github.com/google/uuid"
+	"time"
+)
 
 type Message struct {
 	Recipient string    `json:"recipient,omitempty"`
 	Sender    string    `json:"sender,omitempty"`
 	Body      string    `json:"body,omitempty"`
 	Date      time.Time `json:"date,omitempty"`
+	Provider  string    `json:"provider,omitempty"`
+}
+
+type MessageRepository interface {
+	List(ctx context.Context) ([]*Message, error)
+	Get(id uuid.UUID, ctx context.Context) (*Message, error)
+	Create(entity Message, ctx context.Context) error
 }
--- internal/repository/repository.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "context"
-
-type Repository interface {
-	List(ctx context.Context) (interface{}, error)
-	Get(id interface{}, ctx context.Context) (interface{}, error)
-	Create(entity interface{}, ctx context.Context) (interface{}, error)
-}
