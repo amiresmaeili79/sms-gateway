6acb2db3ba1eaeff584499f8b843991ea898e0a0
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-15 19:43:34 +0330

    postgres repository created

--- internal/repository/pg_repository.go
+++ internal/repository/pg_repository.go
@@ -1,18 +1,13 @@
 package repository
 
 import (
-	"github.com/amir79esmaeili/sms-gateway/internal/cfg"
-	"github.com/amir79esmaeili/sms-gateway/internal/postgres"
 	"github.com/jackc/pgx/v5"
-	"log"
 )
 
 type PostgresRepository struct {
 	db *pgx.Conn
 }
 
-func (p *PostgresRepository) Configure(config *cfg.Config) {
-	var err error
-	p.db, err = postgres.ConnectToDB(config)
-	log.Fatalf("could not configure db, %v \n", err)
+func (p *PostgresRepository) Configure(db *pgx.Conn) {
+	p.db = db
 }
