4dea8760cfb993c04ac39cfd95607d2ecaf68c11
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-15 20:17:19 +0330

    + date field added

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,7 +1,10 @@
 package model
 
+import "time"
+
 type Message struct {
-	Recipient string `json:"recipient,omitempty"`
-	Sender    string `json:"sender,omitempty"`
-	Body      string `json:"body,omitempty"`
+	Recipient string    `json:"recipient,omitempty"`
+	Sender    string    `json:"sender,omitempty"`
+	Body      string    `json:"body,omitempty"`
+	Date      time.Time `json:"date,omitempty"`
 }
