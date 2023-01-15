0c23d962f78bcb0868f62b42c6e7e2d7238a2f87
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-15 15:49:45 +0330

    postgres client added

---
+++ internal/postgres/pg_client.go
@@ -0,0 +1,33 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"github.com/amir79esmaeili/sms-gateway/internal/cfg"
+	"github.com/jackc/pgx/v5"
+	"log"
+)
+
+const UrlSample = "postgres://%s:%s@%s:%s/%s"
+
+// ConnectToDB takes configuration struct and creates a connection to the given Database
+func ConnectToDB(cfg *cfg.Config) (*pgx.Conn, error) {
+	dbUrl := fmt.Sprintf(UrlSample, cfg.DBName, cfg.DBPass, cfg.DBHost, cfg.DBPort, cfg.DBName)
+	conn, err := pgx.Connect(context.Background(), dbUrl)
+	if err != nil {
+		return nil, err
+	}
+	err = conn.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	log.Println("[INFO] Database connected...")
+	return conn, nil
+}
+
+func KillConnection(conn *pgx.Conn) error {
+	if !conn.IsClosed() {
+		return conn.Close(context.Background())
+	}
+	return nil
+}
