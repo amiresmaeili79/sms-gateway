3584749be14799b459f67ee46228a069cc8fc259
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-17 00:43:03 +0330

    added rabbit configuration

--- internal/cfg/struct.go
+++ internal/cfg/struct.go
@@ -1,10 +1,14 @@
 package cfg
 
 type Config struct {
-	DBName  string `env:"DB_NAME,required"`
-	DBUser  string `env:"DB_USER,required"`
-	DBPass  string `env:"DB_PASSWORD,required"`
-	DBHost  string `env:"DB_HOST,required"`
-	DBPort  string `env:"DB_PORT,required"`
-	AppPort string `env:"APP_PORT,required"`
+	DBName     string `env:"DB_NAME,required"`
+	DBUser     string `env:"DB_USER,required"`
+	DBPass     string `env:"DB_PASSWORD,required"`
+	DBHost     string `env:"DB_HOST,required"`
+	DBPort     string `env:"DB_PORT,required"`
+	AppPort    string `env:"APP_PORT,required"`
+	RabbitUser string `env:"RMQ_USER,required"`
+	RabbitPass string `env:"RMQ_PASS,required"`
+	RabbitHost string `env:"RMQ_HOST,required"`
+	RabbitPort string `env:"RMQ_PORT,required"`
 }
